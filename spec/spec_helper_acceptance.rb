4c96d9a7ca728b97dbddb80f98bca8aff436627d
Michael Smith <michael.smith@puppetlabs.com>
2015-05-12 16:29:44 -0700

    (PUP-4567) Update acceptance test
    Prior to this change, only a single test could be run, because `puppet`
    was only installed at the start of the test suite. Change to a pattern
    where every test sets up `puppet`, then cleans up after the test.
    
    Update the existing acceptance test to expect the `puppet-agent`
    package with the `puppet` and `mcollective` services running. Also
    don't expect idempotency to work yet, because we switch from `puppet`
    to `puppet-agent` mid-test.
    
    Add new acceptance tests for upgrading without starting services, and
    upgrading when mcollective is configured.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,19 +5,25 @@ unless ENV['BEAKER_provision'] == 'no'
   # Work-around for BKR-262
   @logger = logger
 
+  # Setup repositories if using a specific SHA
   hosts.each do |host|
-    # Install Puppet
     if ENV['SHA']
       install_puppetlabs_release_repo(host)
       install_puppetlabs_dev_repo(host, 'puppet', ENV['SHA'])
+    end
+  end
+end
 
-      #install_packages_from_local_dev_repo(host, 'puppet')
+def setup_puppet
+  hosts.each do |host|
+    # Install Puppet
+    if ENV['SHA']
       if host['platform'] =~ /debian|ubuntu|cumulus/
         on host, 'apt-get install -y puppet'
       elsif host['platform'] =~ /fedora|el|centos/
         on host, 'yum install -y puppet'
       else
-        raise "No repository installation step for #{host['platform']} yet..."
+        raise "No package installation step for #{host['platform']} yet..."
       end
     elsif host.is_pe?
       install_pe
@@ -25,24 +31,31 @@ unless ENV['BEAKER_provision'] == 'no'
       install_puppet
     end
   end
-end
 
-configure_puppet(:main => {:stringify_facts => false, :parser => 'future'})
+  configure_puppet(:main => {:stringify_facts => false, :parser => 'future'})
 
-RSpec.configure do |c|
   # Project root
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
+  # Install module and dependencies
+  puppet_module_install(:source => proj_root, :module_name => 'agent_upgrade')
+  on hosts, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+  on hosts, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
+end
+
+def teardown_puppet
+  # Note pc1_repo is specific to the module's manifests. This is knowledge we need to clean
+  # the machine after each run.
+  pp = <<-EOS
+package { 'puppet-agent': ensure => absent }
+package { 'puppet': ensure => absent }
+file { ['/etc/puppet', '/etc/puppetlabs', '/etc/mcollective']: ensure => absent, force => true, backup => false }
+yumrepo { 'pc1_repo': ensure => absent }
+  EOS
+  on hosts, "/opt/puppetlabs/bin/puppet apply -e \"#{pp}\""
+end
+
+RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'agent_upgrade')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-    end
-  end
 end
