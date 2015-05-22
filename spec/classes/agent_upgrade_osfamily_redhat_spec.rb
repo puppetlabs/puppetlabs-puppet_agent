1591fafb48405f27abb7bf017fe41587e42d5d39
Michael Smith <michael.smith@puppetlabs.com>
2015-05-22 12:40:56 -0700

    (PUP-4561) Extract platform-specific code
    Before this commit, the `prepare` class was full of platform-specific
    code that needed to be refactored before non-RedHat platforms could be
    supported.
    
    Extract the platform-specific code to `params` and a new class that is
    selected based on the `osfamily` fact.

---
+++ spec/classes/agent_upgrade_osfamily_redhat_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'agent_upgrade::osfamily::redhat' do
+  [['Fedora', 'fedora/f$releasever'], ['CentOS', 'el/$releasever']].each do |os, urlbit|
+    context "with #{os} and #{urlbit}" do
+      let(:facts) {{
+        :osfamily => 'RedHat',
+        :operatingsystem => os,
+        :architecture => 'foo',
+      }}
+
+      it { is_expected.to contain_exec('import-RPM-GPG-KEY-puppetlabs').with({
+        'path'      => '/bin:/usr/bin:/sbin:/usr/sbin',
+        'command'   => 'rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+        'unless'    => 'rpm -q gpg-pubkey-`echo $(gpg --throw-keyids < /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs) | cut --characters=11-18 | tr [A-Z] [a-z]`',
+        'require'   => 'File[/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs]',
+        'logoutput' => 'on_failure',
+      }) }
+
+      ['/etc/pki', '/etc/pki/rpm-gpg'].each do |path|
+        it { is_expected.to contain_file(path).with({
+          'ensure' => 'directory',
+        }) }
+      end
+
+      it { is_expected.to contain_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs').with({
+        'ensure' => 'present',
+        'owner'  => '0',
+        'group'  => '0',
+        'mode'   => '0644',
+        'source' => 'puppet:///modules/agent_upgrade/RPM-GPG-KEY-puppetlabs',
+      }) }
+
+      it { is_expected.to contain_yumrepo('pc1_repo').with({
+        'baseurl' => "https://yum.puppetlabs.com/#{urlbit}/PC1/foo",
+        'enabled' => 'true',
+          'gpgcheck' => '1',
+          'gpgkey' => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+      }) }
+    end
+  end
+end
