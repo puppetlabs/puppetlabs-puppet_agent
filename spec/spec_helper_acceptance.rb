4510aa43465f52eeeee9e514c41af4d80d33f691
Michael Smith <michael.smith@puppetlabs.com>
2015-06-05 16:20:29 -0700

    (PUP-4603) Use Puppet Server 2.1+ for tests
    Use the latest Puppet Server release from Puppet Collections 1 for
    acceptance testing. The tests require at least Puppet Server 2.1 to
    include backwards compatibility with Puppet 3.8.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -27,18 +27,16 @@ PROJ_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 TEST_FILES = File.expand_path(File.join(File.dirname(__FILE__), 'acceptance', 'files'))
 
 unless ENV['BEAKER_provision'] == 'no'
-  # Install repos on hosts
-  hosts.each do |host|
-    install_puppetlabs_release_repo(host)
-
-    if ENV['SHA']
-      step "Setup dev repositories"
-      install_puppetlabs_dev_repo(host, 'puppet', ENV['SHA'])
-    end
-  end
+  # Install release repo for puppet 3.x
+  install_puppetlabs_release_repo default
 
   # Install puppet-server on master
-  install_package master, 'puppet-server'
+  options['is_puppetserver'] = true
+  master['puppetservice'] = 'puppetserver'
+  master['puppetserver-confdir'] = '/etc/puppetlabs/puppetserver/conf.d'
+  master['type'] = 'aio'
+  install_puppet_agent_on master, {}
+  install_package master, 'puppetserver'
   master['use-service'] = true
 
   step "Install module and dependencies"
@@ -48,6 +46,7 @@ unless ENV['BEAKER_provision'] == 'no'
   on master, puppet('module', 'install', 'puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
 
   # Install activemq on master
+  install_puppetlabs_release_repo master
   install_package master, 'activemq'
 
   ['truststore', 'keystore'].each do |ext|
@@ -69,7 +68,12 @@ def parser_opts
   {
     :main => {:stringify_facts => false, :parser => 'future', :color => 'ansi'},
     :agent => {:stringify_facts => false, :cfacter => true, :ssldir => '$vardir/ssl'},
-    :master => {:stringify_facts => false, :cfacter => true},
+  }
+end
+
+def server_opts
+  {
+    :master => {:autosign => true},
   }
 end
 
@@ -104,47 +108,9 @@ def setup_puppet_on(host, opts = {})
   end
 
   if opts[:agent]
-    hostname = on(master, 'facter hostname').stdout.strip
-    fqdn = on(master, 'facter fqdn').stdout.strip
-
-    if master.use_service_scripts?
-      step "Ensure puppet is stopped"
-      # Passenger, in particular, must be shutdown for the cert setup steps to work,
-      # but any running puppet master will interfere with webrick starting up and
-      # potentially ignore the puppet.conf changes.
-      on(master, puppet('resource', 'service', master['puppetservice'], "ensure=stopped"))
-    end
-
     step "Clear SSL on all hosts"
     hosts.each do |host|
-      ssldir = on(host, puppet('agent --configprint ssldir')).stdout.chomp
-      on(host, "rm -rf '#{ssldir}'")
-    end
-
-    step "Master: Start Puppet Master"
-    stop_firewall_on host
-    master_opts = {
-      :main => {
-        :dns_alt_names => "puppet,#{hostname},#{fqdn}",
-      },
-      :__service_args__ => {
-        # apache2 service scripts can't restart if we've removed the ssl dir
-        :bypass_service_script => true,
-      },
-    }
-    with_puppet_running_on(master, master_opts, master.tmpdir('puppet')) do
-      step "Agents: Run agent --test first time to gen CSR"
-      on host, puppet("agent --test --server #{master}"), :acceptable_exit_codes => [1]
-
-      step "Master: sign all certs"
-      on master, puppet("cert --sign --all"), :acceptable_exit_codes => [0,24]
-
-      step "Agents: Run agent --test second time to obtain signed cert"
-      on host, puppet("agent --test --server #{master}"), :acceptable_exit_codes => [0,2]
-    end
-
-    if master.graceful_restarts?
-      on(master, puppet('resource', 'service', master['puppetservice'], "ensure=running"))
+      on(host, "rm -rf '#{host.puppet['ssldir']}'")
     end
   else
     step "Install module and dependencies"
