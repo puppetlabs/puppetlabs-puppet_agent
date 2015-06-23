436fb631d824719bc07870146a0890aeec635d8f
Michael Smith <michael.smith@puppetlabs.com>
2015-06-23 11:40:40 -0700

    (PUP-4478) Fix apt refreshes on Ubuntu
    The module requires that apt update before installing the package.
    Class level dependencies don't seem to enforce that, so add an explicit
    sentinel to force the apt_update exec to run before finishing the repo
    setup.

--- manifests/osfamily/debian.pp
+++ manifests/osfamily/debian.pp
@@ -7,6 +7,14 @@ class puppet_agent::osfamily::debian {
     key        => {
       'id'     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
       'server' => 'pgp.mit.edu',
-    }
+    },
+    notify     => Notify['pc1_repo_force'],
+  }
+
+  # apt_update doesn't inherit the future class dependency, so it
+  # can wait until the end of the run to exec. Force it to happen now.
+  notify { 'pc1_repo_force':
+      message => 'forcing apt update for pc1_repo',
+      require => Exec['apt_update'],
   }
 }
