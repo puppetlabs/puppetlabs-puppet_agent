320f9afda8dc76a1751c3756b3c7df0b52dbbfdf
Britt Gresham <brittcgresham@gmail.com>
2016-01-12 16:34:08 -0800

    (PE-12299) Start services after puppet run
    Before this commit when upgrading solaris 11 agents the services would
    be in a maintenance state where puppet was unable to start them. This
    commit adds in a script which waits for the currently running agent run
    to stop and then calls puppet to ensure that puppet and mcollective are
    running so that users do not have to login to the nodes to perform these
    steps by hand.
    
    This behavior is currently what windows is doing to start puppet and
    mcollective agents when upgrading with the puppet_agent module.

--- manifests/service/solaris.pp
+++ manifests/service/solaris.pp
@@ -13,5 +13,15 @@ class puppet_agent::service::solaris {
     service { 'pe-puppet':
       ensure => stopped,
     }
+  } elsif $::operatingsystem == 'Solaris' and $::operatingsystemmajrelease == '11' {
+    file { "/tmp/solaris_start_puppet.sh":
+      ensure  => file,
+      content => template('puppet_agent/solaris_start_puppet.sh.erb'),
+      mode    => '755',
+    }->
+    exec { 'solaris_start_puppet.sh':
+      command => "/tmp/solaris_start_puppet.sh &",
+      path    => '/usr/bin:/bin:/usr/sbin',
+    }
   }
 }
