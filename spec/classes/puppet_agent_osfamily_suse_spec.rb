219d623dd733296b29898fe8e50a0d0bed68b8b9
Brandon High <brandon.high@puppetlabs.com>
2016-02-10 10:44:58 -0800

    (MODULES-3015) Update SLES specs to match new GPG key import
    Prior to this commit the specs were failing due to changes in
    63e6eb06748bab844463144b5706d9cd2dec44ab to the SLES GPG key
    handling.
    This commit updates the specs to match those changes.

--- spec/classes/puppet_agent_osfamily_suse_spec.rb
+++ spec/classes/puppet_agent_osfamily_suse_spec.rb
@@ -114,7 +114,7 @@ describe 'puppet_agent', :unless => Puppet.version < "3.8.0" || Puppet.version >
           it { is_expected.to contain_exec('import-RPM-GPG-KEY-puppetlabs').with({
             'path'      => '/bin:/usr/bin:/sbin:/usr/sbin',
             'command'   => 'rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
-            'unless'    => 'rpm -q gpg-pubkey-`echo $(gpg --throw-keyids < /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs) | cut --characters=11-18 | tr [A-Z] [a-z]`',
+            'unless'    => 'rpm -q gpg-pubkey-$(echo $(gpg --homedir /root/.gnupg --throw-keyids < /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs) | cut --characters=11-18 | tr [A-Z] [a-z])',
             'require'   => 'File[/etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs]',
             'logoutput' => 'on_failure',
           }) }
