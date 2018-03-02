0dcdfd2ff4825ef87d15a7bda1faf59b0b61e297
tkishel <tkishel@gmail.com>
2018-03-02 08:30:27 -0800

    remove is_pe, add package version and source param, pass to prepare::package

--- manifests/osfamily/windows.pp
+++ manifests/osfamily/windows.pp
@@ -1,11 +1,15 @@
 class puppet_agent::osfamily::windows(
   $package_file_name = undef,
+  $package_version   = undef,
+  $source            = undef,
 ) {
   assert_private()
 
-  if !empty($package_file_name) and $::puppet_agent::is_pe == true {
+  if !empty($package_file_name) {
     class { 'puppet_agent::prepare::package':
       package_file_name => $package_file_name,
+      package_version   => $package_version,
+      source            => $source,
     }
     contain puppet_agent::prepare::package
   }
