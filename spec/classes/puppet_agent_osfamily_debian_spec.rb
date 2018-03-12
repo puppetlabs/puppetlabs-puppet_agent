9dc6cc06b8eb4710e0c69f77e063e09ef24c24a1
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-03-12 15:25:52 +0800

    Revert "(MODULES-6280) ensure compatibility to puppetlabs/apt > 3.0.0 (#268)"
    This reverts commit 3929f0b6db27e73b95413a8de1bbdcb0d8d9a5bf.

--- spec/classes/puppet_agent_osfamily_debian_spec.rb
+++ spec/classes/puppet_agent_osfamily_debian_spec.rb
@@ -5,12 +5,6 @@ describe 'puppet_agent' do
     :lsbdistid => 'Debian',
     :osfamily => 'Debian',
     :lsbdistcodename => 'wheezy',
-    :os => {
-      'name' => 'Debian',
-      'release' => {
-        'full' => '6.0',
-      },
-    },
     :operatingsystem => 'Debian',
     :architecture => 'x64',
       :servername   => 'master.example.vm',
@@ -93,12 +87,6 @@ describe 'puppet_agent' do
           :platform_tag => 'ubuntu-1604-x86_64',
           :operatingsystem => 'Ubuntu',
           :lsbdistcodename => 'xenial',
-          :os => {
-            'name' => 'Ubuntu',
-            'release' => {
-              'full' => '16.04',
-            },
-          },
         })
       }
 
