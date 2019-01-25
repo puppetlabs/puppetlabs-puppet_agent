189f2231e1cbcad8b29e708b76ad5087097aa603
Enis Inan <enis.inan@puppet.com>
2019-01-25 15:41:41 -0800

    (MODULES-8348) Refactor the acceptance test scaffold

--- acceptance/tests/test_upgrade_pc1_to_puppet5.rb
+++ acceptance/tests/test_upgrade_pc1_to_puppet5.rb
@@ -2,29 +2,21 @@ require 'beaker-puppet'
 require_relative '../helpers'
 
 # Tests FOSS upgrades from the latest puppet 4 (the PC1 collection) to puppet 5.5.10.
-# This test will only run if the version of puppet on the master host is less than 5.
 test_name 'puppet_agent class: collection parameter for FOSS upgrades' do
-  master_agent_version = puppet_agent_version_on(master)
+  require_master_collection 'puppet5'
+  exclude_pe_upgrade_platforms
 
-  unless version_is_less(master_agent_version, '5.0.0')
-    skip_test("The puppet-agent package on the master is #{master_agent_version}; Skipping PC1 to puppet5 upgrade test")
-  end
+  set_up_agents_to_upgrade('pc1')
 
-  upgrade_to = '5.5.10'
-
-  manifest = <<-PP
+  step "Upgrading the agents from PC1 to Puppet 5..." do
+    manifest = <<-PP
   class { puppet_agent:
-    package_version => '#{upgrade_to}',
+    package_version => '5.5.10',
     collection      => 'puppet5'
   }
-  PP
-
-  run_foss_upgrade_with_manifest('PC1', manifest) do
-    agents_only.each do |agent|
-      installed_version = puppet_agent_version_on(agent)
-      assert_equal(upgrade_to, installed_version,
-                   "Expected puppet-agent #{upgrade_to} to be installed on #{agent} (#{agent['platform']}), but found '#{installed_version}'")
-    end
+    PP
+    apply_manifest_on_agents(manifest)
   end
-end
 
+  assert_successful_upgrade('5.5.10')
+end
--- acceptance/tests/test_upgrade_puppet5_to_puppet6.rb
+++ acceptance/tests/test_upgrade_puppet5_to_puppet6.rb
@@ -2,28 +2,21 @@ require 'beaker-puppet'
 require_relative '../helpers'
 
 # Tests FOSS upgrades from the latest puppet 5 (the puppet5 collection) to puppet 6.0.0.
-# This test will only run if the version of puppet on the master host is less than 6.
 test_name 'puppet_agent class: collection parameter for FOSS upgrades' do
-  master_agent_version = puppet_agent_version_on(master)
+  require_master_collection 'puppet5'
+  exclude_pe_upgrade_platforms
 
-  unless version_is_less(master_agent_version, '6.0.0')
-    skip_test("The puppet-agent package on the master is #{master_agent_version}; Skipping puppet5 to puppet6 upgrade test")
-  end
+  set_up_agents_to_upgrade('puppet5')
 
-  upgrade_to = '6.0.0'
-
-  manifest = <<-PP
+  step "Upgrading the agents from Puppet 5 to Puppet 6..." do
+    manifest = <<-PP
   class { puppet_agent:
-    package_version => '#{upgrade_to}',
+    package_version => '6.0.0',
     collection      => 'puppet6'
   }
-  PP
-
-  run_foss_upgrade_with_manifest('puppet5', manifest) do
-    agents_only.each do |agent|
-      installed_version = puppet_agent_version_on(agent)
-      assert_equal(upgrade_to, installed_version,
-                   "Expected puppet-agent #{upgrade_to} to be installed on #{agent} (#{agent['platform']}), but found '#{installed_version}'")
-    end
+    PP
+    apply_manifest_on_agents(manifest)
   end
+
+  assert_successful_upgrade('6.0.0')
 end
