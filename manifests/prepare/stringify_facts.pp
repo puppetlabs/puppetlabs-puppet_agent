581666b8c576787b79bf8ed7a59ef76cb73b1eb9
Enis Inan <enis.inan@puppet.com>
2019-01-25 10:53:59 -0800

    (MODULES-8523) Remove unused stringify_facts config class

--- manifests/prepare/stringify_facts.pp
+++
@@ -1,7 +0,0 @@
-# == Class puppet_agent::prepare::stringify_facts
-#
-# A preperation class to configure the stringify_facts setting to false
-#
-class puppet_agent::prepare::stringify_facts {
-  warning('The puppet_agent::prepare::stringify_facts class should only be run on Puppet < 4')
-}
