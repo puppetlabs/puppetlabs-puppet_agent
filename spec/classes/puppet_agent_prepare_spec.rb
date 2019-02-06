73b0e461d3939e76648ee08bd2ce18e669f0c9bf
Casey Williams <caseywilliams@users.noreply.github.com>
2019-02-06 14:23:16 -0800

    (MODULES-4986) Remove deprecated settings for puppet 5
    Several puppet.conf settings were deprecated in puppet 5:
    - app_management
    - ignorecache
    - configtimeout
    - trusted_server_facts
    
    This removes those settings while performing any upgrade to a version of
    puppet 5 or greater.

--- spec/classes/puppet_agent_prepare_spec.rb
+++ spec/classes/puppet_agent_prepare_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
+RSpec.shared_examples 'removes_deprecated_settings' do |min_version, settings_list|
+  describe "deprecated puppet.conf settings in #{min_version}" do
+    let(:params) {{ package_version: min_version }}
+
+    ['', 'agent', 'main', 'master'].each do |section|
+      settings_list.each do |setting|
+        it { is_expected.to contain_ini_setting("#{section}/#{setting}").with_ensure('absent') }
+      end
+    end
+  end
+end
+
 describe 'puppet_agent::prepare' do
   context 'supported operating system families' do
     ['Debian', 'RedHat'].each do |osfamily|
@@ -15,24 +27,8 @@ describe 'puppet_agent::prepare' do
       context "on #{osfamily}" do
         let(:facts) { facts }
 
-        context "when SSL paths do not exist" do
-          let(:facts) {
-            facts.merge({ :puppet_sslpaths => {
-              'privatedir' => { 'path_exists' => false },
-              'privatekeydir' => { 'path_exists' => false },
-              'publickeydir' => { 'path_exists' => false },
-              'certdir' => { 'path_exists' => false },
-              'requestdir' => { 'path_exists' => false },
-              'hostcrl' => { 'path_exists' => false }
-            }})
-          }
-        end
-
-        ['', 'agent', 'main', 'master'].each do |section|
-          let(:params) {{ package_version: '1.10.100' }}
-
-          it { is_expected.to contain_ini_setting("#{section}/pluginsync").with_ensure('absent') }
-        end
+        include_examples('removes_deprecated_settings', '1.4.0', ['pluginsync'])
+        include_examples('removes_deprecated_settings', '5.0.0', ['app_management', 'ignorecache', 'configtimeout', 'trusted_server_facts'])
 
         it { is_expected.to contain_class("puppet_agent::prepare::puppet_config") }
         it { is_expected.to contain_class("puppet_agent::osfamily::#{facts[:osfamily]}") }
