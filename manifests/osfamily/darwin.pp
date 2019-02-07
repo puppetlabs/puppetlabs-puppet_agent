c26c51842bd2a317caf292be40b8edd16994a23b
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2019-02-07 14:15:58 -0800

    (MODULES-8599) Update Darwin class to remove PE/version checks
    MacOS installations now work, FOSS or PE,
    using puppet_agent::prepare::package and the new
    puppet_agent::prepare::package_source class. This commit removes
    the checks for PE and MacOS version from the Darwin class so MacOS
    upgrades are available in FOSS land.

--- manifests/osfamily/darwin.pp
+++ manifests/osfamily/darwin.pp
@@ -1,14 +1,6 @@
 class puppet_agent::osfamily::darwin{
   assert_private()
 
-  if $::macosx_productversion_major !~ /^10\.(12|13)/ {
-    fail("${::macosx_productname} ${::maxosx_productversion_major} not supported")
-  }
-
-  if $::puppet_agent::is_pe != true {
-    fail("${::macosx_productname} upgrades are only supported on Puppet Enterprise")
-  }
-
   if $::puppet_agent::is_pe {
     $pe_server_version = pe_build_version()
     $source = "puppet:///pe_packages/${pe_server_version}/${::platform_tag}/${puppet_agent::package_name}-${puppet_agent::package_version}-1.osx${$::macosx_productversion_major}.dmg"
