8dcaa77ead4a346d1cf6578c4bcdeed98b86b63f
Tobias Kaesser <tk@plista.com>
2020-01-17 15:42:25 +0100

    (MODULES-10452) defensive pattern in prepare/puppet_config.pp creates duplicate-resourcee problems

--- manifests/prepare/puppet_config.pp
+++ manifests/prepare/puppet_config.pp
@@ -8,12 +8,6 @@ class puppet_agent::prepare::puppet_config (
   assert_private()
   $puppetconf = $::puppet_agent::params::config
 
-  if !defined(File[$puppetconf]) {
-    file { $puppetconf:
-      ensure => file,
-    }
-  }
-
   # (minimum agent package version) => (list of deprecated settings)
   $_deprecations = {
     '1.4.0'     => ['pluginsync'],
@@ -22,20 +16,22 @@ class puppet_agent::prepare::puppet_config (
 
   $_pkg_version = getvar('package_version')
 
-  # manage puppet.conf contents, using inifile module
-  $_deprecations.each |$_min_version, $_setting_names| {
-    if (versioncmp($_pkg_version, $_min_version) >= 0) {
-      $_setting_names.each |$_setting_name| {
-        ['', 'master', 'agent', 'main'].each |$_section_name| {
-          $_setting_key = "${_section_name}/${_setting_name}"
+  # manage puppet.conf contents, using inifile module if puppet.conf is managed
+  if defined(File[$puppetconf]) {
+    $_deprecations.each |$_min_version, $_setting_names| {
+      if (versioncmp($_pkg_version, $_min_version) >= 0) {
+        $_setting_names.each |$_setting_name| {
+          ['', 'master', 'agent', 'main'].each |$_section_name| {
+            $_setting_key = "${_section_name}/${_setting_name}"
 
-          if !defined(Ini_setting[$_setting_key]) {
-            ini_setting { $_setting_key:
-              ensure  => absent,
-              section => $_section_name,
-              setting => $_setting_name,
-              path    => $puppetconf,
-              require => File[$puppetconf],
+            if !defined(Ini_setting[$_setting_key]) {
+              ini_setting { $_setting_key:
+                ensure  => absent,
+                section => $_section_name,
+                setting => $_setting_name,
+                path    => $puppetconf,
+                require => File[$puppetconf],
+              }
             }
           }
         }
