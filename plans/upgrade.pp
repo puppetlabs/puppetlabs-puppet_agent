aca59081217ae72797ea0a0f97a7406361652050
Nick Walker <nick.walker@puppetlabs.com>
2020-04-06 11:09:12 -0500

    Add a plan for upgrading puppet-agent

---
+++ plans/upgrade.pp
@@ -0,0 +1,10 @@
+plan puppet_agent::upgrade(
+  TargetSpec $targets,
+  String     $package_version
+){
+  apply($targets, _description => 'upgrade puppet-agent') {
+    class { 'puppet_agent' :
+      package_version => $package_version,
+    }
+  }
+}
