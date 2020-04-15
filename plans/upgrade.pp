60e7f061795b476af0fc09d5fd99a60b13cfddec
Nick Walker <nick.walker@puppetlabs.com>
2020-04-15 15:09:34 -0500

    Add apply_prep to gather facts from targets

--- plans/upgrade.pp
+++ plans/upgrade.pp
@@ -2,6 +2,7 @@ plan puppet_agent::upgrade(
   TargetSpec $targets,
   String     $package_version
 ){
+  apply_prep($targets)
   apply($targets, _description => 'upgrade puppet-agent') {
     class { 'puppet_agent' :
       package_version => $package_version,
