7f48723382711dbdaff61bd12cdd8d8841ba4575
Luchian Nemes <luchian.nemes@puppet.com>
2020-09-23 17:48:43 +0300

    (maint) Fix tests failing on Windows
    Line endings mismatch (`\n` vs `\r\n`) in `*.erb` files caused test
    failures when asserting content of generated puppet catalog. This is
    solved by updating the `.gitatributes` file accordingly.
    
    Some paths got expanded to Windows paths (starting with drive letter
    such as `C:\`) and was causing test failures because of incorrect
    expectation.

--- spec/unit/facter/settings_spec.rb
+++ spec/unit/facter/settings_spec.rb
@@ -1,45 +1,49 @@
 require 'spec_helper'
 
-describe 'puppet_ssldir fact' do
-  subject { Facter.fact(:puppet_ssldir).value }
-  after(:each) { Facter.clear }
+describe 'settings' do
+  let(:location) { File.expand_path('/dev/null') }
 
-  describe 'should point to an existing directory' do
-    it { is_expected.to eq('/dev/null/ssl') }
+  describe 'puppet_ssldir fact' do
+    subject { Facter.fact(:puppet_ssldir).value }
+    after(:each) { Facter.clear }
+
+    describe 'should point to an existing directory' do
+    it { is_expected.to eq("#{location}/ssl") }
+    end
   end
-end
 
-describe 'puppet_config fact' do
-  subject { Facter.fact(:puppet_config).value }
-  after(:each) { Facter.clear }
+  describe 'puppet_config fact' do
+    subject { Facter.fact(:puppet_config).value }
+    after(:each) { Facter.clear }
 
-  describe 'should point to an existing file' do
-    it { is_expected.to eq('/dev/null/puppet.conf') }
+    describe 'should point to an existing file' do
+    it { is_expected.to eq("#{location}/puppet.conf") }
+    end
   end
-end
 
-describe "puppet_stringify_facts fact" do
-  subject { Facter.fact("puppet_stringify_facts".to_sym).value }  
-  after(:each) { Facter.clear }
+  describe "puppet_stringify_facts fact" do
+    subject { Facter.fact("puppet_stringify_facts".to_sym).value }  
+    after(:each) { Facter.clear }
 
-  describe 'should always be false' do
+    describe 'should always be false' do
     it { is_expected.to eq(false) }
+    end
   end
-end
 
-describe "puppet_sslpaths fact" do
-  subject { Facter.fact("puppet_sslpaths".to_sym).value }
-  after(:each) { Facter.clear }
+  describe "puppet_sslpaths fact" do
+    subject { Facter.fact("puppet_sslpaths".to_sym).value }
+    after(:each) { Facter.clear }
 
-  { 'privatedir'    => 'ssl/private',
-    'privatekeydir' => 'ssl/private_keys',
-    'publickeydir'  => 'ssl/public_keys',
-    'certdir'       => 'ssl/certs',
-    'requestdir'    => 'ssl/certificate_requests',
-    'hostcrl'       => 'ssl/crl.pem',
-  }.each_pair do |name, path|
-    describe name do
-      it { is_expected.to include(name => { 'path' => "/dev/null/#{path}", 'path_exists' => false})}
+    { 'privatedir'    => 'ssl/private',
+      'privatekeydir' => 'ssl/private_keys',
+      'publickeydir'  => 'ssl/public_keys',
+      'certdir'       => 'ssl/certs',
+      'requestdir'    => 'ssl/certificate_requests',
+      'hostcrl'       => 'ssl/crl.pem',
+    }.each_pair do |name, path|
+      describe name do
+        it { is_expected.to include(name => { 'path' => "#{location}/#{path}", 'path_exists' => false})}
+      end
     end
   end
 end
