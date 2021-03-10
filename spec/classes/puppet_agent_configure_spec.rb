b92812a8469d4b49d62236509c12bf9c646ecc6d
Reid Vandewiele <reid@puppet.com>
2021-03-10 14:25:52 -0800

    (MODULES-10879) Improve tests for conf. class
    Improve the tests for the puppet_agent::configuration class, per PR
    comments.
    
    Note that mocking of the puppet_agent class is done in order to test
    puppet_agent::configuration. This causes some small annoyances, because
    I can't figure out how to mock returning a value for a scoped variable,
    $puppet_agent::config, without mocking the whole class. I don't want to
    make puppet_agent::configure parameterized, which would be another
    option to ease testing, because I don't want users to have those
    parameters exposed. So opting for slightly harder tests to write.

--- spec/classes/puppet_agent_configure_spec.rb
+++ spec/classes/puppet_agent_configure_spec.rb
@@ -6,29 +6,33 @@ describe 'puppet_agent::configure' do
   let(:pre_condition) do
     [ 'function assert_private() { }',
       'class puppet_agent::params { $config = "/etc/puppetlabs/puppet/puppet.conf" }',
+      'class puppet_agent { $config = $::test_config }',
       'include puppet_agent::params',
       'include puppet_agent',
     ]
   end
 
   context 'with empty config input' do
-    let(:pre_condition) do
-      [ super(),
-        'class puppet_agent { $config = [ ] }',
-      ]
+    let(:node_params) do
+      { 'test_config' => [ ] }
     end
 
     it { is_expected.to compile }
   end
 
   context 'with settings input' do
-    let(:pre_condition) do
-      [ super(),
-        'class puppet_agent { $config = [{ "section" => "agent",
-                                           "setting" => "runinterval",
-                                           "value"   => "30m",
-                                           "ensure"  => "present" }] }',
-      ]
+    let(:node_params) do
+      # Ensure this value matches something that's tested for in
+      # spec/type_aliases/config_spec.rb. It can't be checked here directly
+      # because loading the Puppet_agent::Config type alias would cause
+      # conflicts with our mocking of the puppet_agent class.
+      { 'test_config' => [{ "section" => "agent",
+                            "setting" => "runinterval",
+                            "value"   => "30m",
+                            "ensure"  => "present" },
+                          { "section" => "agent",
+                            "setting" => "environment",
+                            "ensure"  => "absent"}]}
     end
 
     it { is_expected.to contain_ini_setting('puppet-agent-runinterval')
@@ -36,5 +40,9 @@ describe 'puppet_agent::configure' do
                            'setting' => 'runinterval',
                            'value'   => '30m',
                            'ensure'  => 'present'}) }
+    it { is_expected.to contain_ini_setting('puppet-agent-environment')
+                    .with({'section' => 'agent',
+                           'setting' => 'environment',
+                           'ensure'  => 'absent'}) }
   end
 end
