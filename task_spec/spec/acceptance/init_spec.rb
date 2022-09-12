c4712923657e8c2be5b5928ea2d148e0d1a5223f
Josh Cooper <joshcooper@users.noreply.github.com>
2022-09-12 16:26:58 -0700

    (maint) Update recently released OSes
    Enable 6.x -> 6.y upgrades for osx-12-x86_64 since it's been supported for
    multiple 6.x releases. We can't do the same for osx-12-ARM or
    ubuntu-22.04-x86_64, since we don't have immediate plans to releases 6.29.0.
    
    Install 6.28.0 instead of latest/nightly for osx-12-arm and ubuntu 22.04

--- task_spec/spec/acceptance/init_spec.rb
+++ task_spec/spec/acceptance/init_spec.rb
@@ -62,10 +62,8 @@ describe 'install task' do
                          '6.23.0'
                        when %r{osx-12-x86_64}
                          '6.27.1'
-                       when %r{osx-12-arm}
-                         'latest'
-                       when %r{ubuntu-22.04}
-                         'latest'
+                       when %r{osx-12-arm}, %r{ubuntu-22.04}
+                         '6.28.0'
                        else
                          '6.17.0'
                        end
@@ -73,7 +71,7 @@ describe 'install task' do
     # platforms that only have nightly builds available. Once a platform
     # is released, it should be removed from this list.
     case target_platform
-    when %r{ubuntu-22.04}, %r{osx-12-arm}
+    when false
       puppet_6_collection = 'puppet6-nightly'
       puppet_7_collection = 'puppet7-nightly'
     else
@@ -85,7 +83,7 @@ describe 'install task' do
     # Once there a platform has been released more than once, it can be removed
     # from this list.
     multiple_puppet6_versions = case target_platform
-                                when %r{osx-12-x86_64}, %r{osx-12-arm}
+                                when %r{osx-12-arm}
                                   false
                                 when %r{ubuntu-22.04}
                                   false
