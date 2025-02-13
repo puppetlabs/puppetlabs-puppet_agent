513d6c17830d67e95b89ab7827db5226b3b14c72
Josh Cooper <joshcooper@users.noreply.github.com>
2025-02-13 15:57:17 -0800

    Upgrade puppetcore* rpm from yum-puppetcore.puppet.com
    Add optional username and password parameters to the `puppet_agent` class.
    
    If `manage_repo` is true, then add the credentials to the repo config (for RPM
    platforms other than SLES) with secure permissions.
    
    For SLES, add credentials to /etc/zypp/credentials.d/PuppetcoreCreds with secure
    permissions. Also include auth=basic and credentials=PuppetcoreCreds to the
    baseurl.
    
    Update the Dockerfile to install 7.34.0 from yum.puppet.com and upgrade to
    8.11.0 from yum-puppetcore, to verify the module can upgrade agents on amazon
    2023, fedora 40, rocky 8 and sles 15.
    
        export PUPPET_FORGE_TOKEN=...
        docker/bin/upgrade.sh [platform] [from] [to]
    
    where platform is one of amazon, fedora, rocky or sles and from/to are
    puppet-agent versions.
    
    The password is passed to the `docker run` command as an environment variable,
    so that it's not persisted in the docker image.

--- docker/bin/upgrade.sh
+++ docker/bin/upgrade.sh
@@ -8,23 +8,65 @@
 # - PLATFORM: The platform on which the upgrade should occur. This also
 #             supports comma-separated lists. Available:
 #             - `ubuntu`
-#             - `centos`
+#             - `amazon`
+#             - `fedora`
 #             - `rocky`
+#             - `sles`
 #             Default: `ubuntu`
 # - BEFORE: The puppet-agent package version that is installed prior to upgrade.
-#           Default: 1.10.14
+#           Default: 7.34.0
 # - AFTER: The puppet-agent package version that should exist after upgrade.
-#          Default: 6.2.0
+#          Default: 8.1.0
 set -e
 
+if [ -z "${PUPPET_FORGE_TOKEN}" ]; then
+    echo "Environment variable PUPPET_FORGE_TOKEN must be set"
+    exit 1
+fi
+
 cd "$(dirname "$0")/../.."
-platforms=${1:-ubuntu}
+platforms=${1:-rocky}
 before=${2:-7.34.0}
-after=${3:-8.10.0}
+after=${3:-8.11.0}
 for platform in ${platforms//,/ }
 do
-    docker build --rm -f docker/$platform/Dockerfile . -t pa-dev:$platform \
-        --build-arg before=${before}
-    docker run --rm -ti pa-dev:$platform ${after}
+    dockerfile='docker/upgrade/dnf/Dockerfile'
+
+    # REMIND: if (7.35 <= before && before < 8.0) OR (8.11.0 <= before), then install release
+    # package from yum-puppetcore.
+    case $platform in
+        amazon)
+            base_image='amazonlinux:2023'
+            release_package='http://yum.puppet.com/puppet7-release-amazon-2023.noarch.rpm'
+            ;;
+
+        fedora)
+            base_image='fedora:40'
+            release_package='http://yum.puppet.com/puppet7-release-fedora-40.noarch.rpm'
+            ;;
+
+        rocky)
+            base_image='rockylinux/rockylinux:8'
+            release_package='http://yum.puppet.com/puppet7-release-el-8.noarch.rpm'
+            ;;
+
+        sles)
+            base_image='registry.suse.com/suse/sle15:15.6'
+            release_package='http://yum.puppet.com/puppet7-release-sles-15.noarch.rpm'
+            dockerfile='docker/upgrade/sles/Dockerfile'
+            ;;
+
+        *)
+            echo "$0: Usage upgrade.sh [amazon|fedora|rocky|sles] [before] [after]"
+            exit 1
+            ;;
+    esac
+
+    docker build --rm -f ${dockerfile} . -t pa-dev:$platform \
+           --build-arg before=${before} \
+           --build-arg BASE_IMAGE=${base_image} \
+           --build-arg RELEASE_PACKAGE=${release_package}
+
+    docker run -e PUPPET_FORGE_TOKEN --rm -ti pa-dev:$platform ${after}
 done
 echo Complete
