609c3cdacaf71cbe7c918cf24d1b14610aed6afe
David Pearson <pearsondavid1982@gmail.com>
2025-10-10 09:06:59 +0100

    puppet-lint fixes
    tidy manifests to comply with puppet-lint

--- types/config.pp
+++ types/config.pp
@@ -1,8 +1,8 @@
 # @summary This Puppet type alias
 type Puppet_agent::Config = Variant[Struct[{ section          => Enum[main, server, agent, user, master],
-                                            setting          => Puppet_agent::Config_setting,
-                                            value            => String,
-                                            Optional[ensure] => Enum[present, absent] }],
-                                    Struct[{ section          => Enum[main, server, agent, user, master],
-                                            setting          => Puppet_agent::Config_setting,
-                                            Optional[ensure] => Enum[absent] }]]
+    setting          => Puppet_agent::Config_setting,
+    value            => String,
+  Optional[ensure] => Enum[present, absent] }],
+  Struct[{ section          => Enum[main, server, agent, user, master],
+    setting          => Puppet_agent::Config_setting,
+Optional[ensure] => Enum[absent] }]]
